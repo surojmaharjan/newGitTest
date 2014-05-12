a2c46862c7d8a000dfd03903a2f579cac01bf542
surojmaharjan <surojmaharjan@gmail.com>
2014-05-11 22:35:01 -0400

    changes

--- 1.php
+++ 1.php
@@ -6,10 +6,10 @@ echo "<p>This is simple Example of interface</p>";
 
 require_once('interfaces/DotSyntax.interface.php');
 require_once('interfaces/Parsed.interface.php');
-//require_once('interfaces/Compiled.interface.php');
+require_once('interfaces/Compiled.interface.php');
 require_once('PHP.class.php');
-//require_once('Java.class.php');
-//require_once('Python.class.php');
+require_once('Java.class.php');
+require_once('Python.class.php');
 
 $php = new PHP();
 $java = new Java();
